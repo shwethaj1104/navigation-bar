eaabbb6f05d559c2b792aa1df4ae3e28bdadda42
shwethaj1104 <shwethaj1104@gmail.com>
2022-08-12 19:22:23 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Content from './Content'
 function App() {
   return (
-    <>
+    <div className='main-div'>
       <Navbar />
-    </>
+      <Content></Content>
+    </div>
   )
 }
 
---
+++ src/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const content = () => {
+  return (
+    // <div className='image-class'></div>
+    <img src='content.webp'></img>
+  )
+}
+
+export default content
